ad25bfd847e1e481e5f660be057b0e97fdd996fc
Naveen-004 <chinna.nk4@gmail.com>
2023-02-07 14:36:49 +0530

    Added NER and Text summarization

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import requests
-import numpy as np
 import pandas as pd
 import streamlit as st
 from preprocessing import *
 from collections import Counter
-
-ner_api = "https://api-inference.huggingface.co/models/Jean-Baptiste/camembert-ner"
-ner_headers = {"Authorization": "Bearer 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"}
-ts_api = "https://api-inference.huggingface.co/models/sshleifer/distilbart-xsum-12-3"
-ts_headers = {"Authorization": "Bearer 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"}
-tc_api = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
-tc_headers = {"Authorization": "Bearer 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"}
-
-def ts(payload):
-	response = requests.post(ts_api, headers=ts_headers, json=payload)
-	return response.json()
-def ner(payload):
-	response = requests.post(ner_api, headers=ner_headers, json=payload)
-	return response.json()
-def tc(payload):
-    response = requests.post(tc_api, headers=tc_headers, json=payload)
-    return response.json()
 
 st.title('NLP Assist')
 # file_up = st.file_uploader("Upload a file", type='csv')
@@ -41,24 +22,28 @@ if file_up is not None:
         var_select = st.multiselect('Select Variables', obj,
                                   help='Select variables to perform further analysis')
         st.write(len(var_select), 'variables selected')
+        
         # Exploratory Data Analysis
         st.subheader('Exploratory Data Analysis')
         eda_tasks = ['N Words', 'Language Detection', 
-                'Subjectivity', 'Polarity', 
-                'Sentiment']
+                    'Subjectivity', 'Polarity', 
+                    'Sentiment']
         eda_select = st.multiselect('Select EDA Tasks', eda_tasks,
-                    help='EDA Tasks can be performed only on selected variables')
+                    help='EDA Tasks can be performed only on var_select variables')
+        
         # Text Preprocessing
         st.subheader('Text Preprocessing')
         tp_tasks = ['Text cleaning', 'Stop Words removal','Tokenization','Stemming', 'Lemmatization']
         tp_select = st.multiselect('Select Text Preprocessing Tasks', tp_tasks,
-                        help='Text Preprocessing Tasks can be performed only on selected variables and Text preprocessing is nothing but cleaning the text data to improve the quality of data for further analysis.')
+                        help='Text Preprocessing Tasks can be performed only on var_select variables and Text preprocessing is nothing but cleaning the text data to improve the quality of data for further analysis.')
+       
         # NLP models
         st.subheader('NLP Models')
-        models = ['Named Entity Recognition', 'Text summarization', 'Text classification']
+        models = ['Named Entity Recognition', 'Text summarization']
         model = st.selectbox('Select a model', models,
                              help='Select a model to perform further analysis')    
 
+    # EDA
     for i in eda_select:
         if i == 'N Words':
             for i in var_select:
@@ -79,6 +64,8 @@ if file_up is not None:
                 counter_result = Counter(df['sentiment'+' '+str(i)])
                 for key, value in counter_result.items():
                     st.write(str(i), key, 'Sentiment➡️', value)
+
+   # Text Preprocessing                 
     for i in tp_select:
         if i == 'Text cleaning':
             for i in var_select:
@@ -96,6 +83,13 @@ if file_up is not None:
             for i in var_select:
                 df['lemmatized_text'+' '+str(i)] = df[i].apply(lemming)
 
+    # Models                   
+    if model == 'Named Entity Recognition':
+        for i in var_select:
+            df['NER'+' '+str(i)] = df[i].apply(ner)
+    elif model == 'Text summarization':
+        for i in var_select:
+            df['Text Summarization'+' '+str(i)] = df[i].apply(text_summarizer)  
     
     # Display Dataframe        
     button = st.radio('', ('👀 View Complete Dataframe',
@@ -105,18 +99,25 @@ if file_up is not None:
     if button == '👀 View Complete Dataframe':
         st.dataframe(df)
         csv = df.to_csv(index=False)
+        parquet = df.to_parquet(index=False)
     else:
-        # st.dataframe(df.sample(5))
-        selected_col = st.multiselect('Select Variables', df.columns)
-        st.dataframe(df[selected_col])
-        csv = df[selected_col].to_csv(index=False)
+        var_select_col = st.multiselect('Select Variables', df.columns)
+        st.dataframe(df[var_select_col])
+        csv = df[var_select_col].to_csv(index=False)
+        parquet = df[var_select_col].to_parquet(index=False)
 
+    # Download Dataframe
     st.download_button(
         label="Download CSV",
         data=csv,
         file_name='NLP_Assisted.csv',
         mime='text/csv',
     )
-
+    st.download_button(
+        label="Download Parquet",
+        data=parquet,
+        file_name='NLP_Assisted.parquet',
+        mime='text/csv',
+    )
 
     
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,37 @@
 import re
 import spacy
-import pandas as pd
+from heapq import nlargest
 from textblob import TextBlob
-from nltk import word_tokenize
+from string import punctuation
 from langdetect import detect_langs
 from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
+from spacy.lang.en.stop_words import STOP_WORDS
 
 
+# EDA
+def nWords(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    word_count = len(doc.text.split())
+    return word_count
+
+def getLanguages(text):
+    return str(detect_langs(text)).split(':')[0][1:]    
+
+def getSubjectivity(text):
+  return TextBlob(text).sentiment.subjectivity  # type: ignore
+def getPolarity(text):
+  return TextBlob(text).sentiment.polarity  # type: ignore
+
+def getSentiment(score):
+    if score < 0:
+      return "Negative"
+    if score == 0:
+      return "Neutral"
+    else:
+      return "Positive"
+
+# Text Preprocessing
 def textClean(text):
     text = re.sub(r'@[A-Za-z0-9]+', '', text) # removed @mentions
     text = re.sub(r'#','',text) # remove the hash tag
@@ -39,25 +63,55 @@ def lemming(text):
     doc = nlp(text)
     return [token.lemma_ for token in doc]
 
-def nWords(text):
+# Models
+def ner(text):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
-    word_count = len(doc.text.split())
-    return word_count
+    return print([(entity.text, entity.label_) for entity in doc.ents])
 
-def getLanguages(text):
-    return str(detect_langs(text)).split(':')[0][1:]    
+punctuation = punctuation + ' '
+def text_summarizer(text):
+    nlp = spacy.load('en_core_web_sm')
+    stopwords = list(STOP_WORDS)
+    docx = nlp(text)
 
-def getSubjectivity(text):
-  return TextBlob(text).sentiment.subjectivity  # type: ignore
-def getPolarity(text):
-  return TextBlob(text).sentiment.polarity  # type: ignore
+    # tokens and word frequency
+    mytokens = [token.text for token in docx]
+    word_frequencies = {}
+    for word in docx:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
 
-def getSentiment(score):
-    if score < 0:
-      return "Negative"
-    if score == 0:
-      return "Neutral"
-    else:
-      return "Positive"
+    # maximum word frequency
+    max_frequency = max(word_frequencies.values())
 
+    # word frequency normalization
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word]/max_frequency
+
+    # sentence tokens
+    sentence_list = [ sentence for sentence in docx.sents ]
+
+    # sentence score and sorting
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if len(sent.text.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+    # nlargest
+    select_length = int(len(sentence_list)*0.3)
+    summary = nlargest(select_length, sentence_scores, 
+                       key=sentence_scores.get)
+
+    final_summary = [word.text for word in summary]
+    summary = ' '.join(final_summary)
+    return summary
